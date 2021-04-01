a377241056e50db62806c6b0a50c0371bb0c7f3f
qhy2017 <1242359237@qq.com>
2021-04-01 22:37:43 +0800

    first -commit hello

---
+++ src/main/java/com/example/demo/controller/JenkinsDemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsDemoController {
+
+    @RequestMapping(value = "/getHelloWorld",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object getHelloWorld()  {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("jsonObject","HelloWorld");
+        return jsonObject;
+    }
+}
